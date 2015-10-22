f87340303f353a0b0e47de47857a53f477f555da
Paul Scarrone <ninjapanzer@gmail.com>
2015-10-22 18:26:52 -0400

    No need for this gallery thing

--- config.rb
+++ config.rb
@@ -81,7 +81,6 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  #@gallery = Dir.entries("./build/images/gallery/").select {|img| img.include?('.jpg')}
 
   # For example, change the Compass output style for deployment
   # activate :minify_css
